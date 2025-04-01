aa8bb03055e4544f3196d8f787328579ab281d46
meghavarshini <varshinim173@gmail.com>
2025-04-01 18:05:55 +0530

    Initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-[blue] font-tektur">tektur</h1>
+            <h1 className="text-5xl text-[pink] font-montserrat">montserrat</h1>
+            <h1 className="text-5xl text-[red] font-bricolager">bricolage</h1>
+            <h1 className="text-5xl text-[green] font-inter">inter</h1>
+        </div>
+    )
+}
+
+export default Navbar;
