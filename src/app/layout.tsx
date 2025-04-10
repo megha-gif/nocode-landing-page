fb6055d52eb2f22d95e9123d8779616fb1bd3776
meghavarshini <varshinim173@gmail.com>
2025-04-10 16:38:37 +0530

    Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "NoCode Website Builder",
   description: "Build and launch websites with ease, no coding required!",
   icons: {
-    icon: "/favicon.ico", // This is key
+    icon: [
+      { url: "/favicon.ico", type: "image/x-icon" },
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", type: "image/png", sizes: "16x16" },
+    ],
+    apple: "/apple-touch-icon.png",
   },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
@@ -17,12 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700&family=Montserrat:wght@100;200;300;400;500;600;700&family=Bricolage+Grotesque:wght@100;300;400;600&family=Tektur:wght@400;700&display=swap"
           rel="stylesheet"
         />
-      </Head>
+      </head>
       <body>{children}</body>
     </html>
   );
