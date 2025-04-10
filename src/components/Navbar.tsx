180aade5ed762fa489bfb99dc5a15bbe291db664
meghavarshini <varshinim173@gmail.com>
2025-04-10 18:22:19 +0530

    Finally added favicon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -84,7 +84,7 @@ const Navbar = () => {
               2222
               <button
                 onClick={closeMenu}
-                className="text-[19px] text-white p-5 px-[130px] bg-[#007ACC] rounded-[2px] hover:scale-105 font-montserrat"
+                className="text-[19px] text-white p-5 bg-[#007ACC] rounded-[2px] hover:scale-105 font-montserrat"
               >
                 Get Started
               </button>
